acbbdbae7a9fdfb229d0cc1edfafff86da68c6d5
Lyle Dodge <lyledodgegh@users.noreply.github.com>
2022-06-21 12:24:16 -0700

    bash export

--- arc-workshop/account-management/environment-variables-display.ps1
+++ arc-workshop/account-management/environment-variables-display.ps1
@@ -1,5 +1,5 @@
 Write-Host "Current User Environment Variables"
-
+Write-Host "--------------------------------------------------------------------------------"
 Write-Host "myTenantId                 :",$([Environment]::GetEnvironmentVariable('myTenantId', 'User'))
 Write-Host "mySubscriptionId           :",$([Environment]::GetEnvironmentVariable('mySubscriptionId', 'User'))
 Write-Host "myResourceGroup            :",$([Environment]::GetEnvironmentVariable('myResourceGroup', 'User'))
@@ -7,3 +7,16 @@ Write-Host "myServicePrincipalId       :",$([Environment]::GetEnvironmentVariabl
 Write-Host "myServicePrincipalObjectId :",$([Environment]::GetEnvironmentVariable('myServicePrincipalObjectId', 'User'))
 Write-Host "myServicePrincipalSecret   :",$([Environment]::GetEnvironmentVariable('myServicePrincipalSecret', 'User'))
 Write-Host "myAzureLocation            :",$([Environment]::GetEnvironmentVariable('myAzureLocation', 'User'))
+Write-Host "--------------------------------------------------------------------------------"
+Write-Host ""
+Write-Host "Commands to replicate to .profile for Linux"
+Write-Host "--------------------------------------------------------------------------------"
+Write-Host "export myTenantId=",$([Environment]::GetEnvironmentVariable('myTenantId', 'User'))
+Write-Host "export mySubscriptionId=",$([Environment]::GetEnvironmentVariable('mySubscriptionId', 'User'))
+Write-Host "export myResourceGroup=",$([Environment]::GetEnvironmentVariable('myResourceGroup', 'User'))
+Write-Host "export myServicePrincipalId=",$([Environment]::GetEnvironmentVariable('myServicePrincipalId', 'User'))
+Write-Host "export myServicePrincipalObjectId=",$([Environment]::GetEnvironmentVariable('myServicePrincipalObjectId', 'User'))
+Write-Host "export myServicePrincipalSecret=",$([Environment]::GetEnvironmentVariable('myServicePrincipalSecret', 'User'))
+Write-Host "export myAzureLocation=",$([Environment]::GetEnvironmentVariable('myAzureLocation', 'User'))
+Write-Host "--------------------------------------------------------------------------------"
+
