b9464f93e85dff67f7e6dd5569ee3061f756cc07
Lyle Dodge <lyledodgegh@users.noreply.github.com>
2022-08-23 14:23:44 -0700

    setup linux

---
+++ arc-workshop/prerequisites/setup-wsl.sh
@@ -0,0 +1,5 @@
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | sudo bash
+# az extension add --upgrade --yes -n connectedk8s -o none
+# az extension add --upgrade --yes -n k8s-extension -o none
+# az extension add --upgrade --yes -n customlocation -o none
