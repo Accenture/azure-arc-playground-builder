d6891bb765d73ae6781261e006adf0e4d90fdb3f
Lyle Dodge <lyledodgegh@users.noreply.github.com>
2022-08-26 10:24:33 -0700

    done for 3 cluster setup

--- arc-workshop/cluster-creation/fleet-create-local-microk8s.bat
+++ arc-workshop/cluster-creation/fleet-create-local-microk8s.bat
@@ -1,8 +1,6 @@
 rem make sure you create an external network in Hyper-V called "External"
 rem also make sure all your environment variables are set from ..\account-management\environment-variables-set.ps1
 
-call az group create -n %myResourceGroup% -l %myAzureLocation%
-
 call fleet-create-local-microk8s-single.bat seattle-04
 call fleet-create-local-microk8s-single.bat seattle-05
 call fleet-create-local-microk8s-single.bat seattle-06
