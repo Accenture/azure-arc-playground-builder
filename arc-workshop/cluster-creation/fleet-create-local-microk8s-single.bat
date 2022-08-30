058bfebb49685b8909507f2634e7ae2008512893
Lyle Dodge <lyledodgegh@users.noreply.github.com>
2022-08-30 10:30:04 -0700

    added standard extensions

--- arc-workshop/cluster-creation/fleet-create-local-microk8s-single.bat
+++ arc-workshop/cluster-creation/fleet-create-local-microk8s-single.bat
@@ -1,44 +1,48 @@
-set clustername=%1
-set arcclustername=arc-%1
+set clusterid=%1
 
-multipass launch -c 4 -m 16g -d 80g -n %clustername% --network name=External lts
-
-rem pause
+multipass launch -c 6 -m 24g -d 80g -n %clusterid% --network name=External lts
 
 rem tooling
-multipass exec %clustername% -- bash -c "curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash"
-multipass exec %clustername% -- bash -c "curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | sudo bash"
-multipass exec %clustername% -- bash -c "sudo snap install kubectl --classic"
+multipass exec %clusterid% -- bash -c "curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash"
+multipass exec %clusterid% -- bash -c "curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | sudo bash"
+multipass exec %clusterid% -- bash -c "sudo snap install kubectl --classic"
 
 rem microk8s
-multipass exec %clustername% -- bash -c "sudo snap install microk8s --classic --channel=1.24/stable"
-multipass exec %clustername% -- bash -c "sudo iptables -P FORWARD ACCEPT"
-multipass exec %clustername% -- bash -c "mkdir ~/.kube"
-multipass exec %clustername% -- bash -c "sudo usermod -a -G microk8s $USER"
-multipass exec %clustername% -- bash -c "sudo chown -f -R $USER ~/.kube"
-multipass exec %clustername% -- bash -c "microk8s config > ~/.kube/config"
-multipass exec %clustername% -- bash -c "sudo microk8s status --wait-ready"
-multipass exec %clustername% -- bash -c "sudo microk8s enable dns hostpath-storage ingress helm3 dashboard"
+multipass exec %clusterid% -- bash -c "sudo snap install microk8s --classic --channel=1.24/stable"
+multipass exec %clusterid% -- bash -c "sudo iptables -P FORWARD ACCEPT"
+multipass exec %clusterid% -- bash -c "mkdir ~/.kube"
+multipass exec %clusterid% -- bash -c "sudo usermod -a -G microk8s $USER"
+multipass exec %clusterid% -- bash -c "sudo chown -f -R $USER ~/.kube"
+multipass exec %clusterid% -- bash -c "microk8s config > ~/.kube/config"
+multipass exec %clusterid% -- bash -c "sudo microk8s status --wait-ready"
 
-rem https://github.com/kubernetes/dashboard/blob/master/docs/user/access-control/creating-sample-user.md
+ping -n 10 127.0.0.1 > NUL
+multipass exec %clusterid% -- bash -c "sudo microk8s enable dns storage ingress helm3 dashboard"
+
 REM pause here until things are settled down and you see everything is ok and no more deployments are pending!!!
-multipass exec %clustername% -- bash -c "sudo microk8s kubectl get all --all-namespaces"
-
-rem sleep for 1 min
 ping -n 60 127.0.0.1 > NUL
-multipass exec %clustername% -- bash -c "sudo microk8s kubectl get all --all-namespaces"
-rem pause
+multipass exec %clusterid% -- bash -c "sudo microk8s kubectl get all --all-namespaces"
 
 rem azure stuff
-multipass exec %clustername% -- bash -c "az login --service-principal -u %myAzureServicePrincipalId% -p %myAzureServicePrincipalSecret% --tenant %myAzureTenantId%"
-multipass exec %clustername% -- bash -c "az account set --subscription %myAzureSubscriptionId%"
-multipass exec %clustername% -- bash -c "az extension add --upgrade --yes -n connectedk8s -o none"
-multipass exec %clustername% -- bash -c "az extension add --upgrade --yes -n k8s-extension -o none"
-multipass exec %clustername% -- bash -c "az extension add --upgrade --yes -n customlocation -o none"
-multipass exec %clustername% -- bash -c "az provider register --namespace Microsoft.Kubernetes --wait -o none"
-multipass exec %clustername% -- bash -c "az provider register --namespace Microsoft.KubernetesConfiguration --wait -o none"
-multipass exec %clustername% -- bash -c "az provider register --namespace Microsoft.ExtendedLocation --wait -o none"
+multipass exec %clusterid% -- bash -c "az config set extension.use_dynamic_install=yes_without_prompt"
+multipass exec %clusterid% -- bash -c "az login --service-principal -u %myAzureServicePrincipalId% -p %myAzureServicePrincipalSecret% --tenant %myAzureTenantId%"
+multipass exec %clusterid% -- bash -c "az account set --subscription %myAzureSubscriptionId%"
+multipass exec %clusterid% -- bash -c "az extension add --upgrade --yes -n connectedk8s -o none"
+multipass exec %clusterid% -- bash -c "az extension add --upgrade --yes -n k8s-extension -o none"
+multipass exec %clusterid% -- bash -c "az extension add --upgrade --yes -n customlocation -o none"
+multipass exec %clusterid% -- bash -c "az provider register --namespace Microsoft.Kubernetes --wait -o none"
+multipass exec %clusterid% -- bash -c "az provider register --namespace Microsoft.KubernetesConfiguration --wait -o none"
+multipass exec %clusterid% -- bash -c "az provider register --namespace Microsoft.ExtendedLocation --wait -o none"
 
 rem now connect the k8s cluster
-multipass exec %clustername% -- bash -c "az connectedk8s connect --name %arcclustername% --resource-group %myAzureResourceGroup% --custom-locations-oid %myAzureServicePrincipalObjectId%"
-multipass exec %clustername% -- bash -c "az connectedk8s enable-features --name %arcclustername% --resource-group %myAzureResourceGroup% --custom-locations-oid %myAzureServicePrincipalObjectId% --features cluster-connect custom-locations"
+multipass exec %clusterid% -- bash -c "az group create -n %clusterid% -l %myAzureLocation%"
+multipass exec %clusterid% -- bash -c "az connectedk8s connect --name %clusterid% --resource-group %clusterid% --custom-locations-oid %myAzureServicePrincipalObjectId%"
+multipass exec %clusterid% -- bash -c "az connectedk8s enable-features --name %clusterid% --resource-group %clusterid% --custom-locations-oid %myAzureServicePrincipalObjectId% --features cluster-connect custom-locations"
+
+rem setup azure policy, azure defender for cloud, azure monitor for containers
+multipass exec %clusterid% -- bash -c "az monitor log-analytics workspace create -g %clusterid% --workspace-name %clusterid%
+multipass exec %clusterid% -- bash -c "az k8s-extension create --name azuremonitor-containers --cluster-name %clusterid% --resource-group %clusterid% --cluster-type connectedClusters --extension-type Microsoft.AzureMonitor.Containers --configuration-settings logAnalyticsWorkspaceResourceID='/subscriptions/%myAzureSubscriptionId%/resourceGroups/%clusterid%/providers/Microsoft.OperationalInsights/workspaces/%clusterid%'"
+multipass exec %clusterid% -- bash -c "az k8s-extension create --name microsoft.policyinsights --cluster-type connectedClusters --cluster-name %clusterid% --resource-group %clusterid% --extension-type Microsoft.PolicyInsights --name microsoft.policyinsights"
+multipass exec %clusterid% -- bash -c "az k8s-extension create --name microsoft.azuredefender.kubernetes --cluster-type connectedClusters --cluster-name %clusterid% --resource-group %clusterid% --extension-type microsoft.azuredefender.kubernetes"
+
+rem next steps - add app plat extensions and custom location?
