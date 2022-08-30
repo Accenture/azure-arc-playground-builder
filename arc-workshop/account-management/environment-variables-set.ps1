b88458dd6779bd38a1430908d10f12f27a31b89d
Lyle Dodge <lyledodgegh@users.noreply.github.com>
2022-08-30 10:26:11 -0700

    standardized naming conventions

--- arc-workshop/account-management/environment-variables-set.ps1
+++ arc-workshop/account-management/environment-variables-set.ps1
@@ -1,20 +1,20 @@
-$MyTenantId = Read-Host -Prompt 'Input your Tenant Id'
-$MySubscriptionId = Read-Host -Prompt 'Input your Subscription Id'
-$MyResourceGroup = Read-Host -Prompt 'Input your Resource Group'
-$MyServicePrincipalId = Read-Host -Prompt 'Input your Service Principal Id'
-$MyServicePrincipalObjectId = Read-Host -Prompt 'Input your Service Principal ObjectId'
-$MyServicePrincipalSecret = Read-Host -Prompt 'Input your Service Principal Secret'
+$MyAzureTenantId = Read-Host -Prompt 'Input your Tenant Id'
+$MyAzureSubscriptionId = Read-Host -Prompt 'Input your Subscription Id'
+$MyAzureResourceGroup = Read-Host -Prompt 'Input your Resource Group'
+$MyAzureServicePrincipalId = Read-Host -Prompt 'Input your Service Principal Id'
+$MyAzureServicePrincipalObjectId = Read-Host -Prompt 'Input your Service Principal ObjectId'
+$MyAzureServicePrincipalSecret = Read-Host -Prompt 'Input your Service Principal Secret'
 $MyAzureLocation = Read-Host -Prompt "Input your main Azure location"
 $MyAzureADGroup = Read-Host -Prompt "Input your main Azure AD Security Group"
 
 Write-Host "Setting User Environment Variables"
 
-[System.Environment]::SetEnvironmentVariable('myTenantId',$MyTenantId,[System.EnvironmentVariableTarget]::User)
-[System.Environment]::SetEnvironmentVariable('mySubscriptionId',$MySubscriptionId,[System.EnvironmentVariableTarget]::User)
-[System.Environment]::SetEnvironmentVariable('myResourceGroup',$MyResourceGroup,[System.EnvironmentVariableTarget]::User)
-[System.Environment]::SetEnvironmentVariable('myServicePrincipalId',$MyServicePrincipalId,[System.EnvironmentVariableTarget]::User)
-[System.Environment]::SetEnvironmentVariable('myServicePrincipalObjectId',$MyServicePrincipalObjectId,[System.EnvironmentVariableTarget]::User)
-[System.Environment]::SetEnvironmentVariable('myServicePrincipalSecret',$MyServicePrincipalSecret,[System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable('myAzureTenantId',$MyAzureTenantId,[System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable('myAzureSubscriptionId',$MyAzureSubscriptionId,[System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable('myAzureResourceGroup',$MyAzureResourceGroup,[System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable('myAzureServicePrincipalId',$MyAzureServicePrincipalId,[System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable('myAzureServicePrincipalObjectId',$MyAzureServicePrincipalObjectId,[System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable('myAzureServicePrincipalSecret',$MyAzureServicePrincipalSecret,[System.EnvironmentVariableTarget]::User)
 [System.Environment]::SetEnvironmentVariable('myAzureLocation',$MyAzureLocation,[System.EnvironmentVariableTarget]::User)
 [System.Environment]::SetEnvironmentVariable('myAzureADGroup',$MyAzureADGroup,[System.EnvironmentVariableTarget]::User)
 
