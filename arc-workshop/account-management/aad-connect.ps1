5a537498444f0c289bdcd4ea97323f00a7624edf
Lyle Dodge <lyledodgegh@users.noreply.github.com>
2022-08-30 14:22:01 -0700

    notify graph needs powershell 5.x not 7.x

--- arc-workshop/account-management/aad-connect.ps1
+++ arc-workshop/account-management/aad-connect.ps1
@@ -1,2 +1,4 @@
+# Use PowerShell 5.x, Install-Module Microsoft.Graph -Scope AllUsers
+
 Write-Host "Connecting to Microsoft Graph for Tenant Id $Env:myAzureTenantId"
 Connect-MgGraph -TenantId $Env:myAzureTenantId -Scopes "User.ReadWrite.All","Application.ReadWrite.All","Group.ReadWrite.All", "Directory.ReadWrite.All"
