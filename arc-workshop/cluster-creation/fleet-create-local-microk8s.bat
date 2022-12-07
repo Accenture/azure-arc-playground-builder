135f0435148a6e6669bbe7d0ce4f8b0897f343b3
Lyle Dodge <lyledodgegh@users.noreply.github.com>
2022-12-07 11:00:42 -0800

    updated for 1.5 and added minikube

--- arc-workshop/cluster-creation/fleet-create-local-microk8s.bat
+++ arc-workshop/cluster-creation/fleet-create-local-microk8s.bat
@@ -1,6 +1,7 @@
 rem make sure you create an external network in Hyper-V called "External"
 rem also make sure all your environment variables are set from ..\account-management\environment-variables-set.ps1
 
-call fleet-create-local-microk8s-single.bat seattle-04
-call fleet-create-local-microk8s-single.bat seattle-05
-call fleet-create-local-microk8s-single.bat seattle-06
+call fleet-create-local-microk8s-single.bat na-us-wa-sea-04
+call fleet-create-local-microk8s-single.bat na-us-wa-sea-05
+call fleet-create-local-microk8s-single.bat na-us-mi-det-03
+call fleet-create-local-microk8s-single.bat na-us-mi-det-04
